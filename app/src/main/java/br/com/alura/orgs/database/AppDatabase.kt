0d0705e66971e5d0dd20eddf53d03e8f38d19583
Marcio Cesar Gonzaga <marcio.c.gonzaga@gmail.com>
2024-12-02 23:57:05 -0300

    Atualizações do Curso

--- app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -7,19 +7,23 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import br.com.alura.orgs.database.converter.Converters
 import br.com.alura.orgs.database.dao.ProdutoDao
+import br.com.alura.orgs.database.dao.UsuarioDao
 import br.com.alura.orgs.model.Produto
+import br.com.alura.orgs.model.Usuario
 
 @Database(
     entities = [
-        Produto::class
+        Produto::class,
+        Usuario::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = true
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun produtoDao(): ProdutoDao
+    abstract fun usuarioDao(): UsuarioDao
 
     companion object {
         @Volatile
